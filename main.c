74a5c978d8eb08d72d432a1327d8ce4de96fce16
itmojun <dj@itmojun.com>
2022-08-24 10:22:37 +0800

    1.增加了求累加和功能（sum 函数） 2.求 100 的累加和

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+
+int sum(int n);
+
+
 int main()
 {
 	printf("hello, world...\n");
 
+	printf("%d\n", sum(100));
+
 	return 0;
+}
+
+
+int sum(int n)
+{
+	int i, s = 0;
+	for(i = 1; i <= n; i++)
+		s += i;
+
+	return s;
 }
